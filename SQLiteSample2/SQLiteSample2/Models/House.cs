6313353a3d226965b568b44fcb0cea49aad7527f
roamschemer <roamschemer@gmail.com>
2020-03-24 22:54:57 +0900

    Model層にHouseクラスとPersonクラスを追加

---
+++ SQLiteSample2/SQLiteSample2/Models/House.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SQLiteSample2.Models {
+    public class House : BindableBase {
+
+        public ObservableCollection<Person> Person { get; }
+
+    }
+}
---
+++ SQLiteSample2/SQLiteSample2/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLiteSample2.Models {
+    public class Person : BindableBase {
+    }
+}
